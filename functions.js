91236422c4c72050a90d42534bea47608b1c1b77
chcuneo <christian.cuneo@mercadolibre.com>
2016-11-18 22:14:03 -0300

    MapReduce working

--- functions.js
+++ functions.js
@@ -31,21 +31,22 @@ db.personas.mapReduce(
 // Personas involucradas como testigos en la mayor cantidad de casos.
 var map2 = function() {
   var ncasos = 0;
-  for (key in this.involucrado) {
-    if (this.involucrado.rol == 'testigo') {
-      ncasos++;
+  if (this.involucrado) {
+    for (var idx = 0; idx < this.involucrado.length; idx++) {
+        if (this.involucrado[idx].rol == 'testigo') {
+        ncasos++;
+      }
     }
-  }
 
-  emit(this.dni, ncasos);
+  }
+  if (ncasos > 0) { emit(this.dni, ncasos); }
 }
 
 var reduce2 = function(key, values) {
-  return Math.max(values);
+  return values.reduce(function(pv, cv) { return pv + cv; }, 0);
 }
 
 db.personas.mapReduce(map2, reduce2, { out: 'map_reduce_result2' });
-db.map_reduce_result2.find().sort( { value: -1 } );
 
 //Casos en los que se han visto involucradas el mayor numero de personas.
 var map3 = function() {
@@ -57,38 +58,41 @@ var reduce3 = function(key, values) {
 }
 
 db.casos.mapReduce(map3, reduce3, { out: 'map_reduce_result3' });
-db.map_reduce_result3.find().sort( { value: -1 } );
 
 // Cantidad de crimenes por localidad y por ano.
 var map4 = function() {
   emit(this.lugar_suceso.localidad, 1);
-  emit(this.fecha_suceso.getYear(), 1);
+  emit(this.timestamp_suceso.getFullYear(), 1);
 }
 
 var reduce4 = function(key, values) {
-  return Math.sum(values);
+  return values.reduce(function(pv, cv) { return pv + cv; }, 0);
 }
 
 db.casos.mapReduce(map4, reduce4, { out: 'map_reduce_result4' });
 
 // Mayor numero de crimenes cometido por alguna persona.
 var map5 = function() {
-  emit(1, this.culpable.length);
+  if (this.culpable) {
+    emit(1, this.culpable.length);
+  }
 }
 
 var reduce5 = function(key, values) {
-  return Math.max(values);
+  return values.reduce(function(pv, cv) { return (pv > cv) ? pv : cv ; }, 0);
 }
 
 db.personas.mapReduce(map5, reduce5, { out: 'map_reduce_result5' });
 
 // Cantidad total de evidencias por caso.
 var map6 = function() {
-  emit(this.id, this.evidencia.length);
+  if (this.evidencia) {
+    emit(this.id, this.evidencia.length);
+  }
 }
 
 var reduce6 = function(key, values) {
-  return Math.sum(values);
+  return values.reduce(function(pv, cv) { return pv + cv; }, 0);
 }
 
 db.casos.mapReduce(map6, reduce6, { out: 'map_reduce_result6' });
@@ -99,8 +103,15 @@ var map7 = function() {
 }
 
 var reduce7 = function(key, values) {
-  return Math.sum(values);
+  return values.reduce(function(pv, cv) { return pv + cv; }, 0);
 }
 
 db.casos.mapReduce(map7, reduce7, { out: 'map_reduce_result7' })
+
+db.map_reduce_result1.find()
+db.map_reduce_result2.find().sort( { value: -1 } );
+db.map_reduce_result3.find().sort( { value: -1 } );
+db.map_reduce_result4.find()
+db.map_reduce_result5.find()
+db.map_reduce_result6.find()
 db.map_reduce_result7.find().sort( { value: -1 } );
